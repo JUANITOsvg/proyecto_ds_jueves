bb76fac8548fd3067b2accf3b729a0c2149299a6
JUANITOsvg <0juansuazo0@gmail.com>
2025-10-30 22:15:15 -0300

    REPO NUKEADO - Empezando de 0. First commit: 'inicializacion del warehouse (postgres para los amigos)'

--- projects/test/models/test_table_1.sql
+++
@@ -1,4 +0,0 @@
--- models/test_table_1.sql
-SELECT
-    *
-FROM {{ source('public', 'test_table_1') }}
--- superset_config.py
+++
@@ -1,3 +0,0 @@
-import os
-
-SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "replace_me_with_a_secure_random_string")
